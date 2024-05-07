b0c0523c2ac057cdb2b3a16903d114de5fa1e734
Timon-alt <vasdaniwe@gmail.com>
2024-05-07 14:26:56 +0500

    Do reg window

--- app/src/main/java/com/example/soundproject/MainActivity.kt
+++ app/src/main/java/com/example/soundproject/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.soundproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.soundproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            navMenu.setNavigationItemSelectedListener {
+                when(it.itemId) {
+                    R.id.item2 -> Toast.makeText(this@MainActivity, "Item 2", Toast.LENGTH_SHORT).show()
+                }
+                true
+            }
+        }
     }
 }
